2ded5b138a453bfcc9f665539777fff9aa808d16
Hugo <h@hugo.sx>
2017-03-20 19:28:12 +1100

    Finish scaffolding 🚧 👍

--- bin/travultr.js
+++ bin/travultr.js
@@ -1,24 +1,13 @@
 #!/usr/bin/env node
 
-// Native
-// const {resolve} = require('path');
-const pkg = require('./package');
-
 // Packages
-// const chalk = require('chalk');
 const program = require('commander');
+
+// Ours
+const pkg = require('../lib/pkg');
 
 program
   .version(pkg.version)
-  .command('list').description('List servers');
-
-program.parse(process.argv);
-
-// const bin = resolve(__dirname, 'lib', 'v-' + cmd + '.js');
-
-// Prepare process.argv for subcommand
-// process.argv = process.argv.slice(0, 2).concat(args);
-
-// Load sub command
-// With custom parameter to make "pkg" happy
-// require(bin, 'may-exclude');
+  .command('list', 'list available servers')
+  .command('version', 'print travultr version').alias('v')
+  .parse(process.argv);
--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 rm -rf build
 mkdir -p build/{lib,bin}
-find lib/** -type d -exec mkdir -p build/{} \;
-find bin/** -type d -exec mkdir -p build/{} \;
-find lib/** -type f -exec node_modules/.bin/async-to-gen --out-file build/{} {} \;
-find bin/** -type f -exec node_modules/.bin/async-to-gen --out-file build/{} {} \;
-chmod +x build/bin/now.js
+cp -R -f lib/ build/lib/
+cp -R -f bin/ build/bin/
+chmod +x build/bin/travultr.js
