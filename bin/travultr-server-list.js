ce980fc6f2b4f508b59735d7220bd339475f6604
Hugo <h@hugo.sx>
2017-03-20 22:26:42 +1100

    Fix for publishing on npm

---
+++ bin/travultr-server-list.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// Ours
+const list = require('../lib/server/list');
+
+list.then();
--- build.sh
+++ build.sh
@@ -3,8 +3,4 @@ rm -rf build
 mkdir -p build/{lib,bin}
 cp -R -f lib/ build/lib/
 cp -R -f bin/ build/bin/
-
-for i in build/bin/server/*.js; do mv "$i" "${i/server\//travultr-server-}" ; done
-rm -rf build/bin/server/
-
 chmod +x build/bin/travultr.js
