9f01cde739dacad51aad72f861966c803859ab12
Hugo <h@hugo.sx>
2017-03-20 22:00:09 +1100

    Add build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@ rm -rf build
 mkdir -p build/{lib,bin}
 cp -R -f lib/ build/lib/
 cp -R -f bin/ build/bin/
+
+for i in build/bin/server/*.js; do mv "$i" "${i/server\//travultr-server-}" ; done
+rm -rf build/bin/server/
+
 chmod +x build/bin/travultr.js
