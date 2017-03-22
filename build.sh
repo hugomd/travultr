f838b6bf8a40d94e9522ba04bf759f0cb9e8f57c
Hugo <h@hugo.sx>
2017-03-22 22:39:24 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf build
 mkdir -p build/{lib,bin}
-cp -R -f lib/ build/lib/
-cp -R -f bin/ build/bin/
+cp -R -f lib/ build/
+cp -R -f bin/ build/
 chmod +x build/bin/travultr.js
