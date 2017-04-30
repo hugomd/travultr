6185b1e0b1588542de83a45cd1a296ce1259586c
Hugo <h@hugo.sx>
2017-04-30 21:40:08 +1000

    ✨ Add server halt and destroy

--- bin/travultr-server.js
+++ bin/travultr-server.js
@@ -6,4 +6,6 @@ const program = require('commander');
 program
   .command('list', 'list active or pending virtual machines')
   .command('create', 'create a new virtual machine')
+  .command('halt', 'halt a virtual machine')
+  .command('destroy', 'delete a virtual machine')
   .parse(process.argv);
